87d118e92794e51339f04ce3661baa855537ce2b
Carl Myers <cmyers@cmyers.org>
2015-12-31 00:48:56 -0800

    Checkpoint - working on third party license stuff

--- third_party_tools/main/src/misc1/third_party_tools/ivy/IvyCache.java
+++ third_party_tools/main/src/misc1/third_party_tools/ivy/IvyCache.java
@@ -1,9 +1,5 @@
 package misc1.third_party_tools.ivy;
 
-import com.google.common.base.Function;
-import com.google.common.base.Functions;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -11,7 +7,10 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.text.ParseException;
+import java.util.Arrays;
+
 import misc1.commons.ExceptionUtils;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.tuple.Pair;
@@ -19,6 +18,7 @@ import org.apache.ivy.Ivy;
 import org.apache.ivy.core.module.descriptor.DefaultDependencyDescriptor;
 import org.apache.ivy.core.module.descriptor.DefaultModuleDescriptor;
 import org.apache.ivy.core.module.descriptor.DependencyDescriptor;
+import org.apache.ivy.core.module.descriptor.License;
 import org.apache.ivy.core.module.descriptor.ModuleDescriptor;
 import org.apache.ivy.core.module.id.ModuleRevisionId;
 import org.apache.ivy.core.report.ResolveReport;
@@ -29,9 +29,16 @@ import org.apache.ivy.core.settings.IvySettings;
 import org.apache.ivy.plugins.resolver.IBiblioResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import qbt.QbtHashUtils;
 import qbt.QbtTempDir;
 import qbt.QbtUtils;
+
+import com.google.common.base.Function;
+import com.google.common.base.Functions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
 
 public class IvyCache {
     private static final Logger LOGGER = LoggerFactory.getLogger(IvyCache.class);
@@ -49,11 +56,23 @@ public class IvyCache {
             @Override
             public ObjectUtils.Null apply(Path moduleTempDir) {
                 try {
-                    ImmutableList<IvyModuleAndVersion> deps = ivyResolveAndRetrieve(mv, null, ImmutableList.of("master"));
+                    Pair<ImmutableList<IvyModuleAndVersion>, ImmutableMap<IvyModuleAndVersion, ImmutableList<Pair<String, String>>>> depsAndLicenses = ivyResolveAndRetrieve(mv, null, ImmutableList.of("master"));
+                    ImmutableList<IvyModuleAndVersion> deps = depsAndLicenses.getLeft();
                     QbtUtils.writeLines(moduleTempDir.resolve("dependencies"), Iterables.transform(deps, Functions.toStringFunction()));
                     // fetch sources into a separate directory (TODO: change this? have to update eclipsegen)
                     ivyResolveAndRetrieve(mv, moduleTempDir.resolve("files").resolve("jars"), ImmutableList.of("master", "compile", "runtime", "provided"));
                     ivyResolveAndRetrieve(mv, moduleTempDir.resolve("files").resolve("sources"), ImmutableList.of("sources"));
+                    ImmutableList<Pair<String, String>> licenses = depsAndLicenses.getRight().get(mv);
+                    boolean licenseFound = false;
+                    // TODO: if license was overridden in config file, write
+                    // that instead and return
+                    for(Pair<String, String> license : licenses) {
+                        // TODO: try to get license from URL (pair.right)
+                        licenseFound = true;
+                    }
+                    if(!licenseFound) {
+                        // TODO: write "unknown" license file
+                    }
                     return ObjectUtils.NULL;
                 }
                 catch(Exception e) {
@@ -91,7 +110,7 @@ public class IvyCache {
      * https://stackoverflow.com/questions/15598612/simplest-ivy-code-to-programmatically-retrieve-dependency-from-maven-central/22455451#22455451
      *
      */
-    private static ImmutableList<IvyModuleAndVersion> ivyResolveAndRetrieve(IvyModuleAndVersion module, Path destinationPath, ImmutableList<String> configs) throws ParseException, IOException {
+    private static Pair<ImmutableList<IvyModuleAndVersion>, ImmutableMap<IvyModuleAndVersion, ImmutableList<Pair<String, String>>>> ivyResolveAndRetrieve(IvyModuleAndVersion module, Path destinationPath, ImmutableList<String> configs) throws ParseException, IOException {
         LOGGER.debug("Processing configs: " + StringUtils.join(configs, ", "));
         try(QbtTempDir cacheDir = new QbtTempDir()) {
             IvySettings is = new IvySettings();
@@ -137,9 +156,13 @@ public class IvyCache {
             IvyNode ourModule = (IvyNode) rr.getDependencies().get(0);
             ModuleDescriptor ds = ourModule.getDescriptor();
             ImmutableList.Builder<IvyModuleAndVersion> dependencyModuleStrings = ImmutableList.builder();
+            ImmutableMap.Builder<IvyModuleAndVersion, ImmutableList<Pair<String, String>>> licenseMap = ImmutableMap.builder();
+            ModuleRevisionId ourMrid = m.getModuleRevisionId();
+            licenseMap.put(new IvyModuleAndVersion(ourMrid.getOrganisation(), ourMrid.getName(), ourMrid.getRevision()), getLicensesAsStrings(ds.getLicenses()));
             for(DependencyDescriptor depD : ds.getDependencies()) {
                 ModuleRevisionId mrid = depD.getDependencyRevisionId();
-                dependencyModuleStrings.add(new IvyModuleAndVersion(mrid.getOrganisation(), mrid.getName(), mrid.getRevision()));
+                IvyModuleAndVersion imav = new IvyModuleAndVersion(mrid.getOrganisation(), mrid.getName(), mrid.getRevision());
+                dependencyModuleStrings.add(imav);
             }
             // now retrieve
             // ignore classifier because (e.g. sources) should have the same name
@@ -148,7 +171,16 @@ public class IvyCache {
                 String pattern = destinationPath.toAbsolutePath() + "/[artifact]-[revision].[ext]";
                 ivy.retrieve(m.getModuleRevisionId(), pattern, new RetrieveOptions().setConfs(new String[]{"default"}));
             }
-            return dependencyModuleStrings.build();
+            return Pair.of(dependencyModuleStrings.build(), licenseMap.build());
         }
+    }
+
+    private static ImmutableList<Pair<String, String>> getLicensesAsStrings(License[] licenses) {
+        return ImmutableList.copyOf(Iterables.transform(Arrays.asList(licenses), new Function<License, Pair<String, String>>() {
+            @Override
+            public Pair<String, String> apply(License input) {
+                return Pair.of(input.getName(), input.getUrl());
+            }
+        }));
     }
 }
--- third_party_tools/main/src/misc1/third_party_tools/ivy/IvyResult.java
+++ third_party_tools/main/src/misc1/third_party_tools/ivy/IvyResult.java
@@ -1,16 +1,20 @@
 package misc1.third_party_tools.ivy;
 
-import com.google.common.collect.ImmutableList;
 import java.nio.file.Path;
+
 import org.apache.commons.lang3.tuple.Pair;
+
+import com.google.common.collect.ImmutableList;
 
 public final class IvyResult {
     private final ImmutableList<IvyModuleAndVersion> dependencies;
     private final ImmutableList<Pair<Path, Path>> files;
+    private final ImmutableList<Pair<String, String>> licenses; // name, url
 
-    IvyResult(ImmutableList<IvyModuleAndVersion> dependencies, ImmutableList<Pair<Path, Path>> files) {
+    IvyResult(ImmutableList<IvyModuleAndVersion> dependencies, ImmutableList<Pair<Path, Path>> files, ImmutableList<Pair<String, String>> licenses) {
         this.dependencies = dependencies;
         this.files = files;
+        this.licenses = licenses;
     }
 
     public ImmutableList<IvyModuleAndVersion> getDependencies() {
@@ -19,5 +23,9 @@ public final class IvyResult {
 
     public ImmutableList<Pair<Path, Path>> getFiles() {
         return files;
+    }
+
+    public ImmutableList<Pair<String, String>> getLicenses() {
+        return licenses;
     }
 }
