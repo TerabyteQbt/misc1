fa7b0611f0acdab7f64b895eb32a11c870c2ead0
Keith Amling <keith.amling@gmail.com>
2016-01-02 11:58:00 -0800

    Misc1PairUtils.comparator()

--- commons.core/main/src/misc1/commons/tuple/Misc1PairUtils.java
+++ commons.core/main/src/misc1/commons/tuple/Misc1PairUtils.java
@@ -1,6 +1,7 @@
 package misc1.commons.tuple;
 
 import com.google.common.base.Function;
+import java.util.Comparator;
 import org.apache.commons.lang3.tuple.Pair;
 
 public final class Misc1PairUtils {
@@ -30,5 +31,18 @@ public final class Misc1PairUtils {
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public static <A, B> Function<Pair<A, B>, B> rightFunction() {
         return (Function)RIGHT_FUNCTION;
+    }
+
+    public static <A, B> Comparator<Pair<A, B>> comparator(final Comparator<? super A> c1, final Comparator<? super B> c2) {
+        return new Comparator<Pair<A, B>>() {
+            @Override
+            public int compare(Pair<A, B> p1, Pair<A, B> p2) {
+                int r1 = c1.compare(p1.getLeft(), p2.getLeft());
+                if(r1 != 0) {
+                    return r1;
+                }
+                return c2.compare(p1.getRight(), p2.getRight());
+            }
+        };
     }
 }
