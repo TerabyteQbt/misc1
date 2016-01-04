2cc8fbf13ff949575fb62b743d3e2f3feaea499b
Keith Amling <keith.amling@gmail.com>
2016-01-03 20:16:27 -0800

    JDK8: ResourceType

--- commons.core/main/src/misc1/commons/resources/VoidResource.java
+++ commons.core/main/src/misc1/commons/resources/VoidResource.java
@@ -1,16 +1,10 @@
 package misc1.commons.resources;
 
-import com.google.common.base.Function;
-
 public interface VoidResource extends Resource<VoidResource> {
-    public static final ResourceType<RawResource, VoidResource> TYPE = new ResourceType<RawResource, VoidResource>() {
-        public VoidResource wrap(RawResource raw, final Function<FreeScope, VoidResource> onCopyInto) {
-            return new VoidResource() {
-                @Override
-                public VoidResource copyInto(FreeScope scope) {
-                    return onCopyInto.apply(scope);
-                }
-            };
+    public static final ResourceType<RawResource, VoidResource> TYPE = (raw, onCopyInto) -> new VoidResource() {
+        @Override
+        public VoidResource copyInto(FreeScope scope) {
+            return onCopyInto.apply(scope);
         }
     };
 }
